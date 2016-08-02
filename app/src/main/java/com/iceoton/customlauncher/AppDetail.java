c2dc3a8252f20586eff7935a7551a60ee086da39
iceoton <zirconcp@gmail.com>
2016-08-02 16:33:15 +0700

    kick-off

---
+++ app/src/main/java/com/iceoton/customlauncher/AppDetail.java
@@ -0,0 +1,12 @@
+package com.iceoton.customlauncher;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by iceoton on 8/2/2016 AD.
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
